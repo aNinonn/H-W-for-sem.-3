bda237eb338cfe8b1646eb6a9bb9cde4d3795bdd
«nina» <nmrs5201@gmail.com>
2023-07-17 16:45:47 +0300

    With homework for 3 sem

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
-let headers = document.querySelector('h1');
-            headers.style.color = "blue";
+
+const newvar = ["happy", "happy new year","h"]
+newvar.sort()
+console.log(newvar)
+document.write(newvar)
+alert(newvar)
+
+let TruFilm = "Донни Дарко";
+let UserFilm = prompt("Какой мой любимый фильм?");
+FilmQuiz (UserFilm,TruFilm);
+
+function FilmQuiz (UserFilm,TruFilm) {
+    if (UserFilm == TruFilm) { alert("Правильно, вы угадали!")
+    } else {  alert("Неправильно, попробуйте еще раз")
+    };
+};
+
